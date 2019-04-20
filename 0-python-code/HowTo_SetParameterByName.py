870b48449bd799f2b7cb2479146747a93e97db85
franpossetto <franciscopossetto@hotmail.com>
2019-04-20 15:58:07 -0300

    Add Get Parameter Value by Name, Set Parameter Value by Name and Get All Elements of Category Examples

---
+++ 0-python-code/HowTo_SetParameterByName.py
@@ -0,0 +1,31 @@
+"""
+Set Parameter by Name 
+Set one of element's parameters.
+
+TESTED REVIT API: 2016,2017
+
+Author: Francisco Possetto | github.com/franpossetto
+"""
+
+#Imports
+from Autodesk.Revit.DB import Element, Transaction
+
+doc = __revit__.ActiveUIDocument.Document
+uidoc = __revit__.ActiveUIDocument
+t = Transaction(doc, 'Set Parameter by Name')
+
+#Select element from revit.
+selection = [doc.GetElement(x) for x in uidoc.Selection.GetElementIds()]
+
+def set_parameter_by_name(element, parameterName, value):
+	element.LookupParameter(parameterName).Set(value)
+
+#Start Transaction
+t.Start()
+
+for s in selection:
+    #Set a new Comment
+	set_parameter_by_name(s,"Comments", "Good Element")
+
+#End Transaction
+t.Commit()
