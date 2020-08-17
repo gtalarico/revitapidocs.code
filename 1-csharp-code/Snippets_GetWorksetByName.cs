aa5380b85b560d1920889650969953f6cbe2a07d
franpossetto <13245192+franpossetto@users.noreply.github.com>
2020-08-17 13:54:00 -0300

    Code snippets for cs (#26)
    * Add Snippets_GetWorksetByName.cs and Snippets_HideCategory.cs
    
    * Delete blank lines
    
    * Add new line at the end of the file

---
+++ 1-csharp-code/Snippets_GetWorksetByName.cs
@@ -0,0 +1,21 @@
+﻿/*
+Get workset by name.
+TESTED REVIT API: 2020
+Author: Francisco Possetto | github.com/franpossetto
+This file is shared on www.revitapidocs.com
+For more information visit http://github.com/gtalarico/revitapidocs
+License: http://github.com/gtalarico/revitapidocs/blob/master/LICENSE.md
+*/
+
+using System.Linq;
+using System.Collections.Generic;
+using Autodesk.Revit.DB;
+
+public Workset GetWorksetByName(Document doc, string WorksetName)
+{
+    IList<Workset> worksets = new FilteredWorksetCollector(doc).OfKind(WorksetKind.UserWorkset).ToWorksets();
+    Workset workset = worksets.Where(x => x.Name == WorksetName).FirstOrDefault();
+
+    //if the workset does not exist, return the first one.
+    return (workset != null) ? workset : worksets.FirstOrDefault();
+}
---
+++ 1-csharp-code/Snippets_HideCategory.cs
@@ -0,0 +1,31 @@
+﻿/*
+Hide Category.
+TESTED REVIT API: 2020
+Hide / Unhide Category in the active view.
+Author: Francisco Possetto | github.com/franpossetto
+This file is shared on www.revitapidocs.com
+For more information visit http://github.com/gtalarico/revitapidocs
+License: http://github.com/gtalarico/revitapidocs/blob/master/LICENSE.md
+*/
+
+using Autodesk.Revit.DB;
+
+public void HideCategory(Document doc, string categoryName, bool hide)
+{
+    View activeView = doc.ActiveView;
+    Categories categories = doc.Settings.Categories;
+    Category category = null;
+
+    // Get the category by name
+    foreach (Category cat in categories)
+        if (cat.Name == categoryName) category = cat;
+
+    if (category == null) return;
+
+    using (Transaction t = new Transaction(doc, "Hide/Unhide Category"))
+    {
+        t.Start();
+        activeView.SetCategoryHidden(category.Id, hide);
+        t.Commit();
+    }
+}
